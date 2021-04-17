51e8d886f0d355812babd890e9f69abf77ed5f0b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-04-18 01:25:18 +0900

    C언어 scanf_s() 연습하기!(2)

--- C_scanf_s_3.c
+++ C_scanf_s_3.c
@@ -5,13 +5,13 @@ int main(void)
 	int a, all, s, m, h;
 
 	printf("시간 입력(초) : ");
-	a=scanf_s("%d", &s);
+	a=scanf_s("%d", &s); // 사용자로부터 초 입력받기
 
-	all = s;
+	all = s; // all에 입력받은 초로 초기화하기
 
-	if (a != 1)
+	if (a != 1) // 만약 입력받은 것이 조건에 맞게 입력된 1개가 아니라면
 	{
-		printf("입력 오류\n");
+		printf("입력 오류\n"); // "입력 오류"를 출력하기
 		return 1;
 	}
 
